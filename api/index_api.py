3b32be8e54fb55872b55cb5e08de017f9875894c
tangtao <tangtao@ascs.tech>
2024-05-17 13:59:54 +0800

    feat: add

--- api/index_api.py
+++ api/index_api.py
@@ -1,8 +1,13 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, jsonify
 
 index_pb = Blueprint('index_api', __name__)
 
 
 @index_pb.route('/index')
 def index():
-    return "pass123"
+    response = {
+        "code": 200,
+        "message": "Success",
+        "data": "pass123"
+    }
+    return jsonify(response)
