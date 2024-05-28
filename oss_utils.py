196be8f2e6088ca40e1564f8de65a020be8f465a
tangtao <tangtao@ascs.tech>
2024-05-28 22:39:45 +0800

    fix: update image oss

--- oss_utils.py
+++ oss_utils.py
@@ -9,9 +9,11 @@ class OSSClient:
         self.auth = oss2.Auth(access_key_id, access_key_secret)
         self.bucket = oss2.Bucket(self.auth, endpoint, bucket_name)
 
-    def list_files(self, max_keys=10):
+    def list_files(self, max_keys=None):
         files = []
         result = self.bucket.list_objects(max_keys=max_keys)
+        # 收集文件信息到字典中
+        file_info_dict = {}
         for obj in result.object_list:
             if obj.key.endswith('.jpg') or obj.key.endswith('.png') or obj.key.endswith('.webp'):
                 file_name = os.path.basename(obj.key)
@@ -19,14 +21,33 @@ class OSSClient:
                 content_type = detailed_obj.headers['Content-Type']
                 last_modified = datetime.utcfromtimestamp(obj.last_modified)
                 path = '/' + obj.key
-                file_info = Image(
+
+                # 将文件信息存储到字典中
+                file_info_dict[file_name] = {
+                    'path': path,
+                    'last_modified': last_modified,
+                    'size': obj.size,
+                    'content_type': content_type
+                }
+        # 获取数据库中已有的文件名集合
+        existing_file_names = {file.name for file in Image.query.all()}
+        # 批量处理文件信息
+        files_to_add = []
+        for file_name, file_info in file_info_dict.items():
+            if file_name not in existing_file_names:
+                # 如果文件名不存在数据库中，则添加文件到待添加列表
+                file_to_add = Image(
                     name=file_name,
-                    path=path,
-                    last_modified=last_modified,
-                    size=obj.size,
-                    content_type=content_type
+                    path=file_info['path'],
+                    last_modified=file_info['last_modified'],
+                    size=file_info['size'],
+                    content_type=file_info['content_type']
                 )
-                db.session.add(file_info)
-                db.session.commit()
-                files.append(file_info)
+                files_to_add.append(file_to_add)
+        # 一次性添加所有待添加的文件到会话中
+        if files_to_add:
+            db.session.add_all(files_to_add)
+            db.session.commit()
+            files.extend(files_to_add)
         return files
+
