b62d323d4b779dd419d71b4db35738ebf73567b9
tangtao <tangtao@ascs.tech>
2024-07-31 11:17:37 +0800

    fix: cancel music /api

--- api/wedding_api.py
+++ api/wedding_api.py
@@ -4,7 +4,7 @@ from model.music import Music
 wedding_api_pb = Blueprint('wedding_api', __name__)
 
 
-@wedding_api_pb.route('/api/wedding/music/list', methods=['GET'])
+@wedding_api_pb.route('/wedding/music/list', methods=['GET'])
 def get_playlists():
     music_records = Music.query.all()
     playlists = [music.to_dict() for music in music_records]
